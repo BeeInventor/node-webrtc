0fb364d53aff1a6dc6700af66fb941211eef9200
ike <ike.chang@sideeffect.dev>
2023-10-27 23:08:48 +0800

    Update build-webrtc.sh

--- scripts/build-webrtc.sh
+++ scripts/build-webrtc.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-set -e
+#set -e
 
+#set -v
+
+#export PATH=$DEPOT_TOOLS:$PATH
+
+#export TARGETS="webrtc libjingle_peerconnection"
+#if [[ "$(uname)" == "Linux" && "$TARGET_ARCH" == arm* ]]; then
+#  export TARGETS="$TARGETS pc:peerconnection libc++ libc++abi"
+#fi
+#if [[ "$(uname)" == "Darwin" ]]; then
+#  export TARGETS="$TARGETS libc++"
+#fi
+
+#if [ -z "$PARALLELISM" ]; then
+#  ninja $TARGETS
+#else
+#  ninja $TARGETS -j $PARALLELISM
+#fi
+
+#######
+
+#!/bin/bash
+
+set -e
 set -v
 
-export PATH=$DEPOT_TOOLS:$PATH
-
 export TARGETS="webrtc libjingle_peerconnection"
-if [[ "$(uname)" == "Linux" && "$TARGET_ARCH" == arm* ]]; then
-  export TARGETS="$TARGETS pc:peerconnection libc++ libc++abi"
-fi
-if [[ "$(uname)" == "Darwin" ]]; then
-  export TARGETS="$TARGETS libc++"
-fi
+case "$(uname -s)" in
+  Darwin*)
+  export TARGETS="$TARGETS libc++ libc++abi"
+esac
 
 if [ -z "$PARALLELISM" ]; then
   ninja $TARGETS
