7d4ccd70f36dd73878aeabdfa9af740ab5aded0d
ike <ike.chang@sideeffect.dev>
2023-10-28 14:30:29 +0800

    Update configure-webrtc.sh

--- scripts/configure-webrtc.sh
+++ scripts/configure-webrtc.sh
@@ -11,16 +11,18 @@ cd ${SOURCE_DIR}
 if [ "$(uname)" == "Linux" ]; then
 if [ "$TARGET_ARCH" == "arm" ]; then
   python build/linux/sysroot_scripts/install-sysroot.py --arch=arm
-  strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX
-  ls -la /usr/lib/x86_64-linux-gnu/libstdc++.so.6
-  ln -sf /usr/lib/x86_64-linux-gnu/libstdc+.so.6 build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
-  ls -la build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
 elif [ "$TARGET_ARCH" == "arm64" ]; then
   python build/linux/sysroot_scripts/install-sysroot.py --arch=arm64
 else
   python build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
 fi
 fi
+
+# link proper libstdc++.so.6
+strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX
+ls -la /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+ln -sf /usr/lib/x86_64-linux-gnu/libstdc+.so.6 build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
+ls -la build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
 
 # NOTE(mroberts): Running hooks generates this file, but running hooks also
 # takes too long in CI; so do this manually.
