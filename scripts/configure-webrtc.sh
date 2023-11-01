5f9fd72aef54dcb62b96ac6d978d0a3357291d47
ike <ike.chang@sideeffect.dev>
2023-11-01 21:24:49 +0800

    Update configure-webrtc.sh

--- scripts/configure-webrtc.sh
+++ scripts/configure-webrtc.sh
@@ -33,12 +33,8 @@ fi
 # /app/node-webrtc/build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/bin/../lib/libstdc++.so.6:
 # version `GLIBCXX_3.4.30' not found (required by /lib/x86_64-linux-gnu/libicuuc.so.72)
 
-# inspect
-# #strings /app/node-webrtc/build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
-
 # link proper libstdc++.so.6
-
-rm /app/node-webrtc/build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
-ln -sf /usr/lib/x86_64-linux-gnu/libstdc+.so.6 /app/node-webrtc/build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
+#rm /app/node-webrtc/build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
+#ln -sf /usr/lib/x86_64-linux-gnu/libstdc+.so.6 /app/node-webrtc/build/external/libwebrtc/download/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6
 
 gn gen ${BINARY_DIR} "--args=${GN_GEN_ARGS}"
